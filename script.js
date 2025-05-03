36b35b04385e99a1f464448a7bb2edec2b43b2e3
Vi <viopera@outlook.com>
2025-05-03 23:57:15 +0200

    added buttons on select for obs

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let leftScore = 0;
 let rightScore = 0;
 
-function increment(add, side) {
-    side === 'left' ? leftScore += (add === '+' ? 1 : -1) : rightScore += (add === '+' ? 1 : -1);
+function increment(value, side) {
+    side === 'left' ? leftScore += (value) : rightScore += (value);
     updateScores();
 }
 
@@ -26,4 +26,12 @@ function changeBackground() {
     document.body.style.backgroundImage = `url('${selectedBackground}')`;
 }
 
+function moveSelection(direction) {
+    let selectBox = document.getElementById("backgrounds");
+    let currentIndex = selectBox.selectedIndex;
+    let newIndex = currentIndex + direction;
 
+    if (newIndex >= 0 && newIndex < selectBox.options.length) {
+        selectBox.selectedIndex = newIndex;
+    }
+}
